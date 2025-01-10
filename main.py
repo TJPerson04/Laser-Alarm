e9e98448e9a1cc4701ad03623b1f7a459dc0c8de
Tyler DiLoreto <HiPerson929@gmail.com>
2025-01-10 14:10:00 -0500

    Added volume control

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import time
 
 SOUND_DEVICE = "front:CARD=Device,DEV=0"
 ALARM_FILE = "alarm.wav"
+VOLUME = 100  # Out of 100
 
 GPIO.setmode(GPIO.BCM)
-os.system("amixer set Master 100%")
+os.system("amixer set Master " + str(VOLUME) + "%")
 
 while True:
     GPIO.setup(4, GPIO.IN)
