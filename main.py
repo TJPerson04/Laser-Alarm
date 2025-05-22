3abe0cd85c39df47e68bf8f7f7c5ea52dc5e51f2
Tyler DiLoreto <HiPerson929@gmail.com>
2025-05-22 12:59:29 -0400

    Added clock as a constant

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import time
 SOUND_DEVICE = "front:CARD=Device,DEV=0"
 ALARM_FILE = "alarm.wav"
 VOLUME = 100  # Out of 100
+CLOCK = 0.1  # Seconds to wait between checking the sensor(s)
 
 GPIO.setmode(GPIO.BCM)
 os.system("amixer set Master " + str(VOLUME) + "%")
@@ -19,4 +20,4 @@ while True:
     print(input)
     if (input):
         os.system("aplay --device " + SOUND_DEVICE + " " + ALARM_FILE)
-    time.sleep(0.5)
+    time.sleep(CLOCK)
