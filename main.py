7c19cf358ce3ee3c33f9e7d9a9a278608ecfde33
Tyler DiLoreto <HiPerson929@gmail.com>
2025-08-01 13:13:37 -0400

    Increased time alarm goes off if the IR sensor is tripped

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ SOUND_DEVICE = "front:CARD=Device,DEV=0"
 ALARM_FILE = "alarm.wav"
 VOLUME = 95  # Out of 100
 CLOCK = 0.05  # Seconds to wait between checking the sensor(s)
+IR_MULT = 3  # How many times to loop the alarm if the IR sensor is triggered
 
 GPIO.setmode(GPIO.BCM)
 os.system("amixer set Master " + str(VOLUME) + "%")
@@ -23,6 +24,11 @@ while True:
     print("Laser:", laserIn)
     print("IR:", irIn)
     
-    if (laserIn or irIn):
+    if (laserIn):
         os.system("aplay --device " + SOUND_DEVICE + " " + ALARM_FILE)
+    
+    # Run the alarm for longer if the IR sensor is tripped
+    if (irIn):
+        for i in range(IR_MULT):
+            os.system("aplay --device " + SOUND_DEVICE + " " + ALARM_FILE)
     time.sleep(CLOCK)
